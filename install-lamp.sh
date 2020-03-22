3284c00515b9417d265ef6fd2532806478f9091d
Eugeny Nosenko <info@coders.studio>
2020-03-22 21:16:21 +0300

    Maria DB 7.4

--- install-lamp.sh
+++ install-lamp.sh
@@ -8,7 +8,13 @@ sudo apt upgrade
 
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository "deb [arch=amd64,arm64,ppc64el] http://mariadb.mirror.liquidtelecom.com/repo/10.4/ubuntu $(lsb_release -cs) main"
+
 sudo apt update
+
+
 
 sudo apt install -y apache2
 sudo apt install -y mariadb-server
