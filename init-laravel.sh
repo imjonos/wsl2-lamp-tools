d8700b88ba589428b91b01261cc64f67d1030047
Eugeny Nosenko <info@coders.studio>
2020-03-23 13:35:43 +0300

    Update init-laravel.sh

--- init-laravel.sh
+++ init-laravel.sh
@@ -3,14 +3,17 @@
 # Created by Eugeny Nosenko https://coders.studio
 # Feel free to modify it
 
+cp ./.env.example ./.env
+php artisan key:generate
+
 sudo chmod -R 775 ./bootstrap
 sudo chmod -R 775 ./storage
 sudo chgrp -R www-data ./bootstrap
 sudo chgrp -R www-data ./storage
-cp ./.env.example ./.env
+
 composer install
 php artisan vendor:publish
-php artisan key:generate
+
 php artisan migrate
 php artisan db:seed
 
