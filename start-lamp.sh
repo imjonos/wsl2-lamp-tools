01fe1cfb3644e07d2d9b345a6f0da94cd64bcc73
Eugeny Nosenko <info@coders.studio>
2020-03-25 14:19:27 +0300

    Create start-lamp.sh

---
+++ start-lamp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script is used for Install LAMP on Ubuntu.
+# Created by Eugeny Nosenko https://coders.studio
+# Feel free to modify it
+
+sudo wsl2-hosts
+sudo service apache2 start
+sudo service mysql start
+sudo service redis-server start
+sudo service ssh start --force
