1032ac7f4f5eca2c05ca3f8a74a014cba69a50d7
Eugeny Nosenko <info@coders.studio>
2020-03-25 14:16:02 +0300

    Update wsl2-hosts.sh

--- wsl2-hosts.sh
+++ wsl2-hosts.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-export WINIP=$(cat /etc/resolv.conf | grep 'nameserver' | cut -f 2 -d ' ') export 
-WSLIP=$(ip addr show eth0 | grep 'inet ' | cut -f 6 -d ' ' | cut -f 1 -d '/')
+# This script is used for Install LAMP on Ubuntu.
+# Created by Eugeny Nosenko https://coders.studio
+# Feel free to modify it
 
-echo $WSLIP
-hosts=( "test.com" "test2.com" )
+#Add your hosts
+hosts=( "wsl2", "test.com", "test2.com" )
+
+export WINIP=$(cat /etc/resolv.conf | grep 'nameserver' | cut -f 2 -d ' ')
+export WSLIP=$(ip addr show eth0 | grep 'inet ' | cut -f 6 -d ' ' | cut -f 1 -d '/')
 
 for host in "${hosts[@]}"
 do
@@ -17,6 +21,16 @@ do
         echo "$WSLIP  $host\n" >> /mnt/c/Windows/System32/drivers/etc/hosts
     fi
 done
+
+rm /tmp/hosts
+if grep "winhost" /mnt/c/Windows/System32/drivers/etc/hosts
+then
+    cp /mnt/c/Windows/System32/drivers/etc/hosts /tmp/hosts
+    sed -i "s/.*winhost/$WINIP  winhost/" /tmp/hosts
+    cat /tmp/hosts > /mnt/c/Windows/System32/drivers/etc/hosts
+else
+    echo "$WINIP  winhost\n" >> /mnt/c/Windows/System32/drivers/etc/hosts
+fi
 
 cat /mnt/c/Windows/System32/drivers/etc/hosts
 sudo cp /mnt/c/Windows/System32/drivers/etc/hosts /etc/hosts
